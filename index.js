3798291a649e1ecd7ee93ba07295809ddb7976be
Suraj Rao <suraj1101@gmail.com>
2017-03-25 20:51:55 +0530

    test coverages

--- index.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello world");
-const color = require('color')
-
-const redHexa = color({ r: 255, g: 0, b: 0 }).hex()
-
-console.log(redHexa)
---
+++ src/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+// @flow
+import Dog from './dog'
+
+const str = 'ES6'
+console.log(`Hello ${str}`)
+
+const toby = new Dog('Toby')
+
+console.log(toby.bark())
