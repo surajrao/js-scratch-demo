e389f9ab83d1f0e6522cf6637c54cae01f9ca60e
Suraj Rao <suraj1101@gmail.com>
2017-05-07 21:41:12 +0530

    react redux

--- src/server/index.js
+++ src/server/index.js
@@ -3,8 +3,14 @@
 import compression from 'compression'
 import express from 'express'
 
-import { APP_NAME, STATIC_PATH, WEB_PORT } from '../shared/config'
-import { isProd } from '../shared/util'
+import {
+  APP_NAME,
+  STATIC_PATH,
+  WEB_PORT,
+} from '../shared/config'
+import {
+  isProd,
+} from '../shared/util'
 import renderApp from './render-app'
 
 const app = express()
@@ -19,5 +25,6 @@ app.get('/', (req, res) => {
 
 app.listen(WEB_PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' : '(development)'}.`)
+  console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' :
+    '(development).\nKeep "yarn dev:wds" running in an other terminal'}.`)
 })
